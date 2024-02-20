2dca7dddbc143458bc1c5624cf6c819fe3ea936a
Kamil Pomykała <kpomykala2002@gmail.com>
2024-02-20 21:37:24 +0100

    feat: Genre Controller Tests

--- backend/tests/Feature/Http/Controllers/GenreControllerTest.php
+++ backend/tests/Feature/Http/Controllers/GenreControllerTest.php
@@ -2,19 +2,109 @@
 
 namespace Tests\Feature\Http\Controllers;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
+use App\Models\Album;
+use App\Models\Genre;
 
-class GenreControllerTest extends TestCase
+class GenreControllerTest extends ControllerWithAuthTestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function test_get_genres()
     {
-        $response = $this->get('/');
+        Genre::factory(3)->create();
+
+        $response = $this->get('/genres');
 
         $response->assertStatus(200);
+        $response->assertJsonCount(3, 'data');
+    }
+
+    public function test_get_genre()
+    {
+        $genre = Genre::factory()->create();
+
+        $response = $this->get("genres/{$genre->id}");
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'data' => [
+                'id' => $genre->id,
+                'name' => $genre->name,
+                'description' => $genre->description,
+            ]
+        ]);
+    }
+
+    public function test_get_genres_albums()
+    {
+        $genre = Genre::factory()->create();
+        $genre->albums()->saveMany([
+            Album::factory()->make(),
+            Album::factory()->make(),
+        ]);
+
+        $response = $this->get("genres/{$genre->id}/albums");
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(2, 'data');
+    }
+
+    public function test_store_genre()
+    {
+        $response = $this->post("genres", [
+            'name' => 'Genre 1',
+            'description' => 'Description 1',
+        ]);
+
+        $response->assertStatus(201);
+        $response->assertJson([
+            'data' => [
+                'name' => 'Genre 1',
+                'description' => 'Description 1',
+            ]
+        ]);
+    }
+
+    public function test_update_genre()
+    {
+        $genre = Genre::factory()->create();
+
+        $response = $this->put("genres/{$genre->id}", [
+            'name' => 'Genre 2',
+            'description' => 'Description 2',
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'data' => [
+                'id' => $genre->id,
+                'name' => 'Genre 2',
+                'description' => 'Description 2',
+            ]
+        ]);
+    }
+
+    public function test_can_add_album_to_genre()
+    {
+        $genre = Genre::factory()->create();
+        $album = Album::factory()->create();
+
+        $response = $this->post("genres/{$genre->id}/albums/{$album->id}");
+
+        $response->assertStatus(204);
+        $this->assertDatabaseHas('album_genre', [
+            'album_id' => $album->id,
+            'genre_id' => $genre->id,
+        ]);
+    }
+
+    public function test_delete_genre()
+    {
+        $genre = Genre::factory()->create();
+
+        $response = $this->delete("genres/{$genre->id}");
+
+        $response->assertStatus(204);
+        $this->assertSoftDeleted('genres', [
+            'id' => $genre->id,
+        ]);
     }
 }
