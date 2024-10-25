a190c007ea2855c958ab8c33d126bbb20fc05ff3
Kamil Pomykała <kamil@kamilpomykala.dev>
2024-10-25 19:20:36 +0200

    feat(frontend): 404 page

---
+++ frontend/pages/404.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="flex flex-col items-center justify-center w-full min-h-screen">
+    <p class=" text-zinc-500">
+      Are you lost little one?
+    </p>
+    <h1 class="text-9xl font-black">404</h1>
+      <NuxtLink to="/" class="underline text-lg mt-2 hover:text-accent-primary transition-colors">Go back home</NuxtLink>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
