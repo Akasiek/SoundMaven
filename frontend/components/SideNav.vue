e93e3e22a1a92d439f2a3f98af17dc646addeafd
Kamil Pomykała <kamil@kamilpomykala.dev>
2024-10-25 19:37:15 +0200

    feat(frontend): SideNav logo as a link to homepage

--- frontend/components/SideNav.vue
+++ frontend/components/SideNav.vue
@@ -2,10 +2,10 @@
 </script>
 
 <template>
-<nav class="fixed inset-y-0 left-0 w-64 bg-dark-primary py-5 px-5 space-y-6">
-  <div>
+<nav class="fixed inset-y-0 left-0 w-64 bg-dark-primary py-6 px-5 space-y-6">
+  <NuxtLink class="block" to="/">
     <NuxtImg src="/images/logo.svg" alt="SoundMaven logo"/>
-  </div>
+  </NuxtLink>
 
   <div>
     <input type="search" class="border-2 border-accent-primary bg-transparent rounded-lg w-full font-serif px-2.5 py-1.5 text-sm ring-0 focus:ring-0 focus:outline-0 shadow-[0_0_0_#4EFFA6] focus:shadow-[0_0_10px_#4EFFA6AA] transition-all" placeholder="Search...">
