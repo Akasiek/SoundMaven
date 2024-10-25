36096894ddb37a2781a5abcdae2b254f2e457469
Kamil Pomykała <kamil@kamilpomykala.dev>
2024-10-25 22:33:46 +0200

    refactor(frontend): Hero to component

---
+++ frontend/components/album/view/Hero.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type Album from "~/interfaces/Album";
+
+const { album } = defineProps<{
+  album: Album;
+}>();
+</script>
+
+<template>
+  <section id="hero" class="flex gap-10">
+    <NuxtImg v-if="album.cover_image" class="h-80" :src="album.cover_image" :alt="`${album.title} album cover`"/>
+
+    <div class="font-serif my-auto">
+      <h2 class="text-2xl"> {{ album.artist.name }} </h2>
+      <h1 class="text-4xl font-bold mb-2"> {{ album.title }} </h1>
+      <h4 class="text-lg text-zinc-400 mb-2">
+        {{ album.release_date }} | {{ album.type }}
+      </h4>
+      <p v-if="album.genres">
+        <template v-for="(genre, index) in album.genres" :key="genre.id">
+            <span class="italic">
+              {{ genre.name }}
+            </span>
+          <template v-if="index !== album.genres.length - 1"> •</template>
+        </template>
+      </p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/pages/album/[slug].vue
+++ frontend/pages/album/[slug].vue
@@ -18,24 +18,6 @@ useHead({
 
 <template>
   <main v-if="album" class="py-12">
-    <section id="hero" class="flex gap-10 mx-auto w-fit">
-      <NuxtImg v-if="album.cover_image" class="h-80" :src="album.cover_image" :alt="`${album.title} album cover`"/>
-
-      <div class="font-serif my-auto">
-        <h2 class="text-2xl"> {{ album.artist.name }} </h2>
-        <h1 class="text-4xl font-bold mb-2"> {{ album.title }} </h1>
-        <h4 class="text-lg text-zinc-400 mb-2">
-          {{ album.release_date }} | {{ album.type }}
-        </h4>
-        <p v-if="album.genres">
-          <template v-for="(genre, index) in album.genres" :key="genre.id">
-            <span class="italic">
-              {{ genre.name }}
-            </span>
-            <template v-if="index !== album.genres.length - 1"> • </template>
-          </template>
-        </p>
-      </div>
-    </section>
+    <AlbumViewHero :album="album" />
   </main>
 </template>
