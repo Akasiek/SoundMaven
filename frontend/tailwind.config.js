229b46ee9b3aae24ca6fa686d2d8634ba9e6e90a
Kamil Pomykała <kamil@kamilpomykala.dev>
2024-10-25 19:22:25 +0200

    feat(frontend): Fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: [ 'Montserrat', 'sans-serif' ],
+      serif: [ 'Lora', 'serif' ],
+    },
+
+    extend: {
+      colors: {
+        dark: {
+          primary: '#292A33',
+          secondary: '#1B1C22',
+        },
+        light: {
+          primary: '#F3EFF5',
+        },
+        accent: {
+          primary: '#4EFFA6',
+          secondary: '#5E2BFF',
+        },
+      }
+    }
   },
   plugins: [],
 }
