02d6e3a2a8e927c79877a627f30f77ebe88860e4
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-03-02 13:22:31 +0100

    feat: Enhance artist view layout with improved styling and background image
    - Introduced a main layout wrapper to the artist view for better structure.
    - Added a gradient background overlay for a visually appealing effect.
    - Implemented an aspect ratio container for the artist's background image.
    - Enhanced the artist's name presentation with larger, bolder text.

--- backend/resources/views/pages/artist-view.blade.php
+++ backend/resources/views/pages/artist-view.blade.php
@@ -1,3 +1,19 @@
-<div>
-    {{ $artist->name }}
-</div>
+<main class="max-w-7xl mx-auto">
+    <section class="w-full relative">
+        <div class="absolute inset-0 bg-gradient-to-t from-zinc-900 to-transparent"></div>
+        <div class="aspect-video w-full">
+            <img src="{{ $artist->background_image }}" alt="{{ $artist->name }} background" class="w-full h-full object-center object-cover">
+        </div>
+
+        <div class="absolute inset-x-0 bottom-10 mx-10">
+
+            <h1 class="text-6xl font-black text-white">
+                {{ $artist->name }}
+            </h1>
+        </div>
+    </section>
+
+    <section>
+
+    </section>
+</main>
