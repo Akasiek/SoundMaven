f6af30fc43e0b152f5b09c41f4339ffba7887e5b
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-03-17 21:41:12 +0100

    feat: Enhance artist list page layout and interactivity
    - Improved grid spacing for better layout consistency and aesthetics.
    - Updated artist link to include a hover effect and border styling.
    - Added a gradient overlay and a name label for each artist's background image.

--- backend/resources/views/pages/artist-list.blade.php
+++ backend/resources/views/pages/artist-list.blade.php
@@ -1,12 +1,15 @@
 <main class="max-w-7xl mx-auto">
-    <div class="grid grid-cols-4 gap-x-10 gap-y-10">
+    <div class="grid grid-cols-4 gap-x-8 gap-y-16 my-32">
         @foreach ($artists as $artist)
-            <a class="block" href="{{ route('artist.show', $artist->slug) }}">
-
-                <div class="w-full h-48 aspect-video overflow-hidden">
+            <a href="{{ route('artist.show', $artist->slug) }}"
+               class="block border-2 border-zinc-800 overflow-hidden rounded-md hover:border-zinc-600 transition duration-300 ease-in-out hover:scale-102">
+                <div class="w-full h-48 aspect-video overflow-hidden relative">
                     <img src="{{$artist->background_image_preview}}" alt="{{ $artist->name }}" class="w-full h-full object-cover object-center">
+                    <div class="absolute inset-0 bg-gradient-to-t to-50% from-zinc-900 to-transparent"></div>
+                    <div class="absolute inset-x-0 bottom-0 bg-opacity-50 px-4 py-2">
+                        <h2 class="text-zinc-50 text-lg font-bold shadow-zinc-900/50 [text-shadow:_2px_2px_5px_var(--tw-shadow-color)]">{{ $artist->name }}</h2>
+                    </div>
                 </div>
-                {{ $artist->name }}
             </a>
         @endforeach
     </div>
