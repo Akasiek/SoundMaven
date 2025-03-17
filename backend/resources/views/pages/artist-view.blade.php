17f9b7c08bd6dd2d402c12203de95ca2e011d98a
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-03-17 21:00:19 +0100

    feat: Enhance artist view layout with improved styling
    - Updated gradient background for a smoother visual effect.
    - Added bottom margin to artist's name for better spacing.
    - Introduced top and bottom margins for the album list section to improve layout.

--- backend/resources/views/pages/artist-view.blade.php
+++ backend/resources/views/pages/artist-view.blade.php
@@ -1,19 +1,19 @@
 <main class="max-w-7xl mx-auto">
     <section class="w-full relative">
-        <div class="absolute inset-0 bg-gradient-to-t from-zinc-900 to-transparent"></div>
+        <div class="absolute inset-0 bg-gradient-to-t to-50% from-zinc-900 to-transparent"></div>
         <div class="aspect-video w-full">
             <img src="{{ $artist->background_image }}" alt="{{ $artist->name }} background" class="w-full h-full object-center object-cover">
         </div>
 
         <div class="absolute inset-x-0 bottom-10 mx-10">
 
-            <h1 class="text-6xl font-black text-white">
+            <h1 class="text-6xl font-black text-white mb-4">
                 {{ $artist->name }}
             </h1>
         </div>
     </section>
 
-    <section>
+    <section class="my-12">
         @livewire(\App\Livewire\AlbumList::class, ['albums' => $artist->albums, 'showArtist' => false])
     </section>
 </main>
