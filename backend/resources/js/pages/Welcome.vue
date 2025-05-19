ceb6d0ac96baf692f73856e21097b5bb82f4fa70
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-05-19 22:13:18 +0200

    feat: Add Inertia.js support for Laravel and update to Laravel 12

---
+++ backend/resources/js/pages/Welcome.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {Head, Link} from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <div class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] dark:bg-[#0a0a0a] lg:justify-center lg:p-8">
+        Welcome! Welcome! To the SoundMaven!
+
+        <br>
+
+        Hip, hip, hooray! You are now a part of the SoundMaven community. We are thrilled to have you here.
+    </div>
+</template>
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,14 +1,26 @@
 <?php
 
-use App\Livewire\{AlbumView, ArtistList, ArtistView, Welcome};
+// use App\Livewire\{AlbumView, ArtistList, ArtistView, Welcome};
 
-Route::get('/', Welcome::class)->name('home');
+// Route::get('/', Welcome::class)->name('home');
+//
+// Route::name('artist.')->group(function () {
+//     Route::get('/artists', ArtistList::class)->name('index');
+//     Route::get('/artists/{artistId}', ArtistView::class)->name('show');
+// });
+//
+// Route::name('album.')->group(function () {
+//     Route::get('/albums/{albumId}', AlbumView::class)->name('show');
+// });
 
-Route::name('artist.')->group(function () {
-    Route::get('/artists', ArtistList::class)->name('index');
-    Route::get('/artists/{artistId}', ArtistView::class)->name('show');
-});
 
-Route::name('album.')->group(function () {
-    Route::get('/albums/{albumId}', AlbumView::class)->name('show');
-});
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
