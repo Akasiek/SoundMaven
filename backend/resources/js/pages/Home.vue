ed5da44cb97b54e3936480f7a2748c2ec34f95d4
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-05-20 22:29:45 +0200

    feat: Update Home page layout and improve grid spacing
    - Replaced the PrimaryLayout component with a more generic Layout component.
    - Adjusted grid spacing in the Home page for better visual alignment.
    - Removed unnecessary Head component to streamline the template structure.

--- backend/resources/js/pages/Home.vue
+++ backend/resources/js/pages/Home.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { Head } from '@inertiajs/vue3';
 import AlbumCard from "@/components/albums/AlbumCard.vue";
-import PrimaryLayout from "@/layouts/PrimaryLayout.vue";
+import Layout from "@/layouts/Layout.vue";
 
+defineOptions({ layout: Layout })
 interface Props {
     albums: { data: ExtendedAlbum[] };
 }
@@ -11,15 +11,12 @@ defineProps<Props>();
 </script>
 
 <template>
-    <Head title="Welcome"/>
-    <PrimaryLayout>
 
-        <div class="container mx-auto px-4">
-            <div class="grid grid-cols-4 gap-x-5 gap-y-10 pt-10">
+        <div class="container mx-auto">
+            <div class="grid grid-cols-4 gap-x-6 gap-y-12 pt-10">
                 <template v-for="album in albums.data" :key="album.id">
                     <AlbumCard :album/>
                 </template>
             </div>
         </div>
-    </PrimaryLayout>
 </template>
