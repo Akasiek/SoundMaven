28d8e2d6259da50b13b835e96a716de3036a111a
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-05-20 22:29:16 +0200

    feat: Replace PrimaryLayout with Layout component
    - Removed the obsolete PrimaryLayout.vue file to streamline the layout structure.
    - Introduced a new Layout.vue component that incorporates a Navigation component.
    - Updated layout styling to enhance the overall user interface and maintain consistency.

---
+++ backend/resources/js/layouts/Layout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import Navigation from "@/components/Navigation.vue";
+</script>
+
+<template>
+    <Navigation/>
+    <main class="font-serif bg-zinc-900 text-zinc-100 mt-18 px-4 sm:px-6 lg:px-8">
+        <slot />
+    </main>
+</template>
+
+<style scoped>
+
+</style>
