09dce8b503c4e70bad6c59335f4a778b3630a5ab
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-05-20 22:09:03 +0200

    feat: Introduce PrimaryLayout component and remove older layouts
    - Added a new PrimaryLayout component for consistent layout structure.
    - The component includes a main template with background styling.
    - It allows for flexible content placement using slots.

---
+++ backend/resources/js/layouts/PrimaryLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main class="bg-zinc-900 min-h-screen text-zinc-50">
+        <slot />
+
+    </main>
+</template>
+
+<style scoped>
+
+</style>
