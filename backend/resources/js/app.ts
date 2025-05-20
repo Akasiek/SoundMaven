c32ff004e6b2812a7fbec283670254ccb3c2098b
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-05-20 22:16:50 +0200

    refactor: Remove unused components

--- backend/resources/js/app.ts
+++ backend/resources/js/app.ts
@@ -1,11 +1,10 @@
 import '../css/app.css';
 
-import {createInertiaApp} from '@inertiajs/vue3';
-import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
-import type {DefineComponent} from 'vue';
-import {createApp, h} from 'vue';
-import {ZiggyVue} from '../../node_modules/ziggy-js';
-import {initializeTheme} from './composables/useAppearance';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import type { DefineComponent } from 'vue';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from '../../node_modules/ziggy-js';
 
 // Extend ImportMeta interface for Vite...
 declare module 'vite/client' {
@@ -35,6 +34,3 @@ createInertiaApp({
         color: '#4B5563',
     },
 });
-
-// This will set light / dark mode on page load...
-initializeTheme();
