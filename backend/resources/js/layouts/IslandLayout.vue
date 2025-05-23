f0a1f752d6bd3c3bb7c84c28f45a7308c1f4bfd1
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-05-23 20:20:16 +0200

    style: Enhance layout responsiveness in IslandLayout component
    - Increased minimum width of the main content area to improve layout adaptability.
    - Adjusted maximum width to allow for better content presentation on larger screens.

--- backend/resources/js/layouts/IslandLayout.vue
+++ backend/resources/js/layouts/IslandLayout.vue
@@ -6,7 +6,7 @@ import PrimaryLayout from "@/layouts/PrimaryLayout.vue";
 <template>
     <PrimaryLayout>
         <div class="w-full min-h-screen flex items-center justify-center">
-            <main class="bg-zinc-800 p-8 rounded-lg shadow-lg w-full max-w-sm border border-zinc-700">
+            <main class="bg-zinc-800 p-8 rounded-lg shadow-lg min-w-sm max-w-xl border border-zinc-700">
                 <slot/>
             </main>
         </div>
