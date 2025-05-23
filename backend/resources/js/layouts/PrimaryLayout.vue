511e935c8b1448afc75503c342d8e02723469a73
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-05-23 19:29:39 +0200

    feat: Add PrimaryLayout component for improved page structure
    - Introduced a new PrimaryLayout component to manage page structure.
    - Integrated Inertia.js Head for managing document metadata.
    - Set up a responsive design with viewport settings and favicon links.
    - Established a default styling with a dark background and serif font.

---
+++ backend/resources/js/layouts/PrimaryLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/favicon.png">
+    </Head>
+    <div class="font-serif bg-zinc-900 text-zinc-100 min-h-screen">
+        <slot/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
