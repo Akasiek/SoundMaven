d6d6c8247ccafce6b94c19dcea616b31d2801807
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-05-23 20:03:09 +0200

    feat: Add IslandLayout component for enhanced layout structure
    - Introduced a new IslandLayout component to the codebase.
    - Utilizes PrimaryLayout for consistent styling and structure.
    - Provides a centered main content area with a responsive design.

---
+++ backend/resources/js/layouts/IslandLayout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import PrimaryLayout from "@/layouts/PrimaryLayout.vue";
+</script>
+
+<template>
+    <PrimaryLayout>
+        <div class="w-full min-h-screen flex items-center justify-center">
+            <main class="bg-zinc-800 p-8 rounded-lg shadow-lg w-full max-w-sm border border-zinc-700">
+                <slot/>
+            </main>
+        </div>
+    </PrimaryLayout>
+</template>
+
+<style scoped>
+
+</style>
