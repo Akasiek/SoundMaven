94ee75e5ca28e4d495676bcc01ed354837a29db4
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-05-23 19:29:16 +0200

    feat: Enhance Inertia app layout resolution
    - Introduced a default layout for pages that do not specify one.
    - Modified the page resolution logic to eagerly load components.
    - Improved the app's structure by utilizing a centralized AppLayout component.

--- backend/resources/js/app.ts
+++ backend/resources/js/app.ts
@@ -1,15 +1,16 @@
 import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../node_modules/ziggy-js';
+import AppLayout from "@/layouts/AppLayout.vue";
 
 // Extend ImportMeta interface for Vite...
 declare module 'vite/client' {
     interface ImportMetaEnv {
         readonly VITE_APP_NAME: string;
+
         [key: string]: string | boolean | undefined;
     }
 
@@ -23,9 +24,14 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => title ? `${title} - ${appName}` : appName,
-    resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob<DefineComponent>('./pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+    resolve: name => {
+        const pages = import.meta.glob<DefineComponent>('./pages/**/*.vue', {eager: true});
+        let page = pages[`./pages/${name}.vue`]
+        page.default.layout = page.default.layout || AppLayout;
+        return page
+    },
+    setup({el, App, props, plugin}) {
+        createApp({render: () => h(App, props)})
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
