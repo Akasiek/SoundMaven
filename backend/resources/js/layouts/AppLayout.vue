fd62ab4cd57aeb2f79bdaa9c2d57b148db018315
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-05-23 19:29:05 +0200

    feat: Replace Layout component with AppLayout for improved structure
    - Introduced AppLayout component to enhance layout organization.
    - Removed the old Layout.vue file to streamline the codebase.
    - Integrated PrimaryLayout into the new AppLayout for better hierarchy.

---
+++ backend/resources/js/layouts/AppLayout.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Navigation from "@/components/Navigation.vue";
+import PrimaryLayout from "@/layouts/PrimaryLayout.vue";
+</script>
+
+<template>
+    <PrimaryLayout>
+        <Navigation/>
+        <main class="mt-18 px-4 sm:px-6 lg:px-8">
+            <slot/>
+        </main>
+    </PrimaryLayout>
+</template>
+
+<style scoped>
+
+</style>
