1e1370f6eba3a2209f7c7c91a8a9a299c80b9826
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-05-29 20:39:30 +0200

    feat: Enhance NavAvatar Component with Improved Dropdown Styling
    - Added the router import to facilitate logout functionality.
    - Simplified DropdownMenu structure for better readability.
    - Updated DropdownMenuContent and DropdownMenuItem styles for improved visual consistency.
    - Introduced background and border color changes to enhance dropdown appearance.

--- backend/resources/js/components/NavAvatar.vue
+++ backend/resources/js/components/NavAvatar.vue
@@ -1,68 +1,43 @@
 <script setup lang="ts">
-
-import { Link } from "@inertiajs/vue3";
+import { Link, router } from "@inertiajs/vue3";
 import { LogOut } from 'lucide-vue-next';
 import type { User } from "@/types";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/shadcn/ui/avatar";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
 } from "@/components/shadcn/ui/dropdown-menu";
 
 defineProps<{ user: User; }>();
 
 const handleLogout = () => {
-    router.flushAll();
+  router.flushAll();
 };
 </script>
 
 <template>
+  <DropdownMenu :modal="false">
+    <DropdownMenuTrigger as-child>
+      <Avatar class="w-10 h-10">
+        <AvatarImage src="https://github.com/unovue.png" alt="@unovue"/>
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent class="min-w-56 rounded-lg" align="end" :side-offset="4">
+      <!-- TODO -->
+      <DropdownMenuSeparator class="bg-zinc-700"/>
+      <DropdownMenuItem :as-child="true">
+        <Link class="block w-full" method="post" :href="route('logout')" @click="handleLogout" as="button">
+          <LogOut class="mr-2 h-4 w-4 text-zinc-50"/>
+          Log out
+        </Link>
+      </DropdownMenuItem>
 
-
-    <DropdownMenu :modal="false">
-        <DropdownMenuTrigger as-child>
-            <Avatar class="w-10 h-10">
-                <AvatarImage src="https://github.com/unovue.png" alt="@unovue"/>
-                <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent class="min-w-56 rounded-lg" align="end" :side-offset="4">
-            <DropdownMenuGroup>
-                <!--                <DropdownMenuItem>-->
-                <!--                    <User class="mr-2 h-4 w-4"/>-->
-                <!--                    <span>Profile</span>-->
-                <!--                    <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>-->
-                <!--                </DropdownMenuItem>-->
-                <!--                <DropdownMenuItem>-->
-                <!--                    <CreditCard class="mr-2 h-4 w-4"/>-->
-                <!--                    <span>Billing</span>-->
-                <!--                    <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>-->
-                <!--                </DropdownMenuItem>-->
-                <!--                <DropdownMenuItem>-->
-                <!--                    <Settings class="mr-2 h-4 w-4"/>-->
-                <!--                    <span>Settings</span>-->
-                <!--                    <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>-->
-                <!--                </DropdownMenuItem>-->
-                <!--                <DropdownMenuItem>-->
-                <!--                    <Keyboard class="mr-2 h-4 w-4"/>-->
-                <!--                    <span>Keyboard shortcuts</span>-->
-                <!--                    <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>-->
-                <!--                </DropdownMenuItem>-->
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator/>
-            <DropdownMenuItem :as-child="true">
-                <Link class="block w-full" method="post" :href="route('logout')" @click="handleLogout" as="button">
-                    <LogOut class="mr-2 h-4 w-4"/>
-                    Log out
-                </Link>
-            </DropdownMenuItem>
-
-        </DropdownMenuContent>
-    </DropdownMenu>
+    </DropdownMenuContent>
+  </DropdownMenu>
 </template>
 
 <style scoped>
