622799b5794d7117dffd6d623086e77a3a619b7c
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-05-30 20:21:34 +0200

    feat: Enhance AlbumList component with optional date display
    - Updated AlbumList props to accept a `showDate` boolean option.
    - Modified AlbumCard rendering to conditionally display artist and date.
    - Improved flexibility of the AlbumList component for varied album presentations.

--- backend/resources/js/components/albums/AlbumList.vue
+++ backend/resources/js/components/albums/AlbumList.vue
@@ -2,13 +2,13 @@
 
 import AlbumCard from "@/components/albums/AlbumCard.vue";
 
-defineProps<{ albums: ExtendedAlbum[], showArtist: boolean }>();
+defineProps<{ albums: ExtendedAlbum[] | Album[], showArtist?: boolean, showDate?: boolean }>();
 </script>
 
 <template>
   <div class="grid xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-6 gap-y-12">
     <div v-for="album in albums" :key="album.id">
-      <AlbumCard :album="album" :show-artist="showArtist"/>
+      <AlbumCard :album="album" :show-artist="showArtist || true" :show-date="showDate || false"/>
     </div>
   </div>
 </template>
