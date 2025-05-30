0106b68b9c6afc490825b55e9cef43bf49c7b9da
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-05-30 20:21:45 +0200

    feat: Enhance AlbumCard component with extended album support
    - Updated the `album` prop type to support both `ExtendedAlbum` and `Album`.
    - Added conditional rendering for the artist link to check if the album has an artist.
    - Improved TypeScript type casting for better type safety when accessing artist properties.

--- backend/resources/js/components/albums/AlbumCard.vue
+++ backend/resources/js/components/albums/AlbumCard.vue
@@ -1,39 +1,45 @@
 <script setup lang="ts">
 defineProps<{
-    album: ExtendedAlbum;
-    showArtist?: boolean;
-    showDate?: boolean;
+  album: ExtendedAlbum | Album;
+  showArtist?: boolean;
+  showDate?: boolean;
 }>();
 </script>
 
 <template>
-    <a :href="`/album/${album.slug}`" class="block group transition duration-300 ease-in-out">
-        <div
-            class="w-full aspect-square overflow-hidden relative transition duration-300 ease-in-out border-2 rounded-md border-zinc-800 group-hover:border-zinc-600">
-            <img v-if="album.cover_image" :src="album.cover_image" :alt="album.title" class="w-full h-full object-cover object-center"/>
-            <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
-                <rect width="300" height="300" fill="#27262AFF"></rect>
-                <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-family="serif" font-size="26px" fill="#FAFAFAFF">
-                    Missing cover
-                </text>
-            </svg>
-        </div>
+  <a :href="`/album/${album.slug}`" class="block group transition duration-300 ease-in-out">
+    <div
+      class="w-full aspect-square overflow-hidden relative transition duration-300 ease-in-out border-2 rounded-md border-zinc-800 group-hover:border-zinc-600">
+      <img v-if="album.cover_image" :src="album.cover_image" :alt="album.title" class="w-full h-full object-cover object-center"/>
+      <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
+        <rect width="300" height="300" fill="#27262AFF"></rect>
+        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-family="serif" font-size="26px" fill="#FAFAFAFF">
+          Missing cover
+        </text>
+      </svg>
+    </div>
 
-        <div
-            class="grid grid-cols-[1fr_auto] px-3 mt-4 py-3 border-2 rounded-md border-zinc-800 group-hover:border-zinc-600 transition duration-300 ease-in-out">
-            <div class="space-y-1">
-                <h2 class="sm:text-lg font-bold">{{ album.title }}</h2>
-                <a v-show="showArtist || true" :href="route('artists.show', album.artist.slug)" class="hover:underline">
-                    <h3 class="text-sm sm:text-base">{{ album.artist.name }}</h3>
-                </a>
-                <p class="text-xs sm:text-sm font-sans text-zinc-400" v-show="showDate || false">
-                    {{ new Date(album.release_date).toLocaleDateString() }}
-                </p>
-            </div>
+    <div
+      class="grid grid-cols-[1fr_auto] px-3 mt-4 py-3 border-2 rounded-md border-zinc-800 group-hover:border-zinc-600 transition duration-300 ease-in-out">
+      <div class="space-y-1">
+        <h2 class="sm:text-lg font-bold">{{ album.title }}</h2>
 
-            <div :class="`px-4 py-3 text-xl sm:text-2xl md:text-3xl font-black flex justify-center leading-none ${album.rating_color}`">
-                {{ album.average_rating }}
-            </div>
-        </div>
-    </a>
+        <a
+          v-if="showArtist && 'artist' in album"
+          :href="route('artists.show', (album as ExtendedAlbum).artist.slug)"
+          class="hover:underline"
+        >
+          <h3 class="text-sm sm:text-base">{{ (album as ExtendedAlbum).artist.name }}</h3>
+        </a>
+        <p class="text-xs sm:text-sm font-sans text-zinc-400" v-show="showDate || false">
+          {{ new Date(album.release_date).toLocaleDateString() }}
+        </p>
+
+      </div>
+
+      <div :class="`px-4 py-3 text-xl sm:text-2xl md:text-3xl font-black flex justify-center leading-none ${album.rating_color}`">
+        {{ album.average_rating }}
+      </div>
+    </div>
+  </a>
 </template>
