496692696f679d4e04d1f6a1aa3fab52201b9bc2
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-05-30 20:16:13 +0200

    feat: Replace AlbumCard with AlbumList for enhanced album display
    - Introduced a new AlbumList component for better album layout.
    - Replaced the individual AlbumCard instances with AlbumList in Home.vue.
    - Improved template structure for displaying albums with additional properties.

---
+++ backend/resources/js/components/albums/AlbumList.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import AlbumCard from "@/components/albums/AlbumCard.vue";
+
+defineProps<{ albums: ExtendedAlbum[], showArtist: boolean }>();
+</script>
+
+<template>
+  <div class="grid xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-6 gap-y-12">
+    <div v-for="album in albums" :key="album.id">
+      <AlbumCard :album="album" :show-artist="showArtist"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- backend/resources/js/pages/Home.vue
+++ backend/resources/js/pages/Home.vue
@@ -1,19 +1,17 @@
 <script setup lang="ts">
-import AlbumCard from "@/components/albums/AlbumCard.vue";
+import AlbumList from "@/components/albums/AlbumList.vue";
 
 interface Props {
-    albums: { data: ExtendedAlbum[] };
+  albums: { data: ExtendedAlbum[] };
 }
 
 defineProps<Props>();
 </script>
 
 <template>
-        <div class="container mx-auto">
-            <div class="grid grid-cols-4 gap-x-6 gap-y-12 pt-10">
-                <template v-for="album in albums.data" :key="album.id">
-                    <AlbumCard :album/>
-                </template>
-            </div>
-        </div>
+  <div class="container mx-auto">
+    <div class="py-12">
+      <AlbumList :albums="albums.data" :show-artist="true"/>
+    </div>
+  </div>
 </template>
