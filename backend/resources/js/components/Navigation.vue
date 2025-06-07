0dc87c399e1a9970eb925994dbe76d2ef8d866b3
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-06-07 20:18:58 +0200

    feat: Improve Navigation Component Structure and User Authentication Handling
    - Enhanced readability by adjusting indentation and formatting in the Navigation.vue file.
    - Updated user authentication handling to allow for null user values for better safety.
    - Maintained existing functionality while improving code clarity and organization.

--- backend/resources/js/components/Navigation.vue
+++ backend/resources/js/components/Navigation.vue
@@ -6,44 +6,44 @@ import NavAvatar from "@/components/NavAvatar.vue";
 
 
 const links = [
-    { name: 'Home', url: route('home') },
-    { name: 'Artists', url: route('artists.list') },
-    { name: 'Contact', url: '/contact' },
+  { name: 'Home', url: route('home') },
+  { name: 'Artists', url: route('artists.list') },
+  { name: 'Contact', url: '/contact' },
 ];
 
 const page = usePage<SharedData>();
-const user = page.props.auth.user as User;
+const user = page.props?.auth?.user as User | null;
 
 
 </script>
 
 <template>
-    <nav class="fixed inset-x-0 top-0 w-full h-18 bg-zinc-800 px-4 sm:px-6 lg:px-8 z-50">
-        <div class="container w-full mx-auto flex justify-between items-center h-full">
+  <nav class="fixed inset-x-0 top-0 w-full h-18 bg-zinc-800 px-4 sm:px-6 lg:px-8 z-50">
+    <div class="container w-full mx-auto flex justify-between items-center h-full">
 
-            <a class="block w-48" href="/">
-                <img src="/images/logo.svg" alt="Logo">
-            </a>
+      <a class="block w-48" href="/">
+        <img src="/images/logo.svg" alt="Logo">
+      </a>
 
-            <div class="flex items-center gap-x-5">
-                <Link
-                    v-for="link in links"
-                    :key="link.name"
-                    :href="link.url"
-                    class="font-bold hover:text-zinc-300 hover:underline"
-                >
-                    {{ link.name }}
-                </Link>
+      <div class="flex items-center gap-x-5">
+        <Link
+          v-for="link in links"
+          :key="link.name"
+          :href="link.url"
+          class="font-bold hover:text-zinc-300 hover:underline"
+        >
+          {{ link.name }}
+        </Link>
 
-                <hr class="border-l border-zinc-600 h-6">
+        <hr class="border-l border-zinc-600 h-6">
 
-                <NavAvatar v-if="user" :user="user"/>
-                <div v-else>
-                    <Link :href="route('login')" class="font-bold hover:text-zinc-300 hover:underline">
-                        Log In
-                    </Link>
-                </div>
-            </div>
+        <NavAvatar v-if="user" :user="user"/>
+        <div v-else>
+          <Link :href="route('login')" class="font-bold hover:text-zinc-300 hover:underline">
+            Log In
+          </Link>
         </div>
-    </nav>
+      </div>
+    </div>
+  </nav>
 </template>
