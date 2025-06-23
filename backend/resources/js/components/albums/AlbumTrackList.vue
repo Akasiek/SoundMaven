9400d08efdbbf591c8b8d4fdb725ffcbf1fc530a
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-06-23 20:56:43 +0200

    feat: Add AlbumTrackList component to display album tracks
    - Introduced AlbumTrackList component for better track organization.
    - Enhanced Show.vue to include AlbumTrackList, displaying album tracks.
    - Implemented disc grouping for tracks based on their disc numbers.

---
+++ backend/resources/js/components/albums/AlbumTrackList.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+const { album } = defineProps<{ album:  ExtendedAlbum }>();
+
+const discs = album.tracks.reduce((acc, { disc, ...track }) => {
+  acc[disc] = acc[disc] || [];
+  acc[disc].push({ disc, ...track });
+  return acc;
+}, {} as Record<number, Track[]>);
+</script>
+
+<template>
+  <aside class="bg-zinc-850 border-2 border-zinc-800 shadow-xl rounded-lg p-6" v-if="Object.keys(discs).length > 0">
+    <h2 class="text-2xl font-bold mb-5">
+      Tracks
+    </h2>
+    <div class="space-y-5">
+      <div v-for="(tracks, discNumber) in discs" :key="discNumber" class="space-y-2">
+        <h3 class="text-lg font-semibold text-zinc-100" v-if="Object.keys(discs).length > 1"> Disc {{ discNumber }} </h3>
+        <ol class="pl-5 list-decimal text-zinc-400 space-y-2 text-base">
+          <li v-for="track in tracks" :key="track.id">
+            <span class="text-zinc-50">{{ track.title }}</span>
+            <span v-if="track.length_in_minutes" class="text-zinc-400"> ({{ track.length_in_minutes }})</span>
+          <hr class="border-zinc-700 mt-2 -ml-5">
+          </li>
+        </ol>
+      </div>
+    </div>
+  </aside>
+</template>
--- backend/resources/js/pages/album/Show.vue
+++ backend/resources/js/pages/album/Show.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 
 import AlbumHero from "@/components/albums/AlbumHero.vue";
+import AlbumTrackList from "@/components/albums/AlbumTrackList.vue";
 
 const props = defineProps<{ album: { data: ExtendedAlbum } }>();
 const album = props.album.data;
@@ -9,5 +10,15 @@ const album = props.album.data;
 <template>
   <div class="pt-12">
     <AlbumHero :album="album"/>
+  </div>
+
+  <div class="grid grid-cols-[2fr_1fr] gap-12 container mx-auto mt-12">
+
+    <section class="bg-zinc-850 border-2 border-zinc-800 shadow-xl rounded-lg p-6">
+
+    </section>
+
+    <AlbumTrackList :album="album"/>
+
   </div>
 </template>
