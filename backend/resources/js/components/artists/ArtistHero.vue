5ad83ea6877a2569ec989c4c9150e791ae22bc7f
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-06-28 22:33:14 +0200

    feat: Add ArtistHero component and integrate into Show.vue
    - Introduced a new ArtistHero component to display artist details with gradients and improved styling.
    - Replaced the existing artist header section in Show.vue with the new ArtistHero component for better modularity.

---
+++ backend/resources/js/components/artists/ArtistHero.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+defineProps<{ artist: Artist }>()
+</script>
+
+<template>
+  <section class="w-full relative scale-105">
+    <div id="artist-gradient-bottom" class="absolute -inset-1"></div>
+    <div id="artist-gradient-right" class="absolute -inset-1"></div>
+    <div id="artist-gradient-left" class="absolute -inset-1"></div>
+    <div id="artist-radial-gradient" class="absolute inset-0"></div>
+    <div class="aspect-video w-full">
+      <img :src="artist.background_image" :alt="`${artist.name} background`" class="w-full h-full object-center object-cover">
+    </div>
+
+    <div class="absolute inset-x-0 bottom-10 mx-16">
+      <h1 class="text-6xl font-black text-white mb-12">
+        {{ artist.name }}
+      </h1>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+#artist-radial-gradient {
+  background: radial-gradient(
+    ellipse at top,
+    transparent 0%,
+    transparent 50%,
+    hsla(240, 5.88%, 10%, 0.322) 63%,
+    hsla(240, 5.88%, 10%, 0.738) 76%,
+    hsl(240, 5.88%, 10%) 100%
+  );
+}
+
+#artist-gradient-bottom {
+  background: linear-gradient(0deg,
+  hsl(240, 5.88%, 10%) 0%,
+  hsla(240, 5.88%, 10%, 0.738) 9.5%,
+  hsla(240, 5.88%, 10%, 0.541) 17%,
+  hsla(240, 5.88%, 10%, 0.382) 23.5%,
+  hsla(240, 5.88%, 10%, 0.278) 28.25%,
+  hsla(240, 5.88%, 10%, 0.194) 32.5%,
+  hsla(240, 5.88%, 10%, 0.126) 36.5%,
+  hsla(240, 5.88%, 10%, 0.075) 40.1%,
+  hsla(240, 5.88%, 10%, 0.042) 43.05%,
+  hsla(240, 5.88%, 10%, 0.021) 45.5%,
+  hsla(240, 5.88%, 10%, 0.008) 47.6%,
+  hsla(240, 5.88%, 10%, 0.002) 49.1%,
+  hsla(240, 5.88%, 10%, 0) 50%
+  );
+}
+
+#artist-gradient-right {
+  background: linear-gradient(90deg,
+  hsl(240, 5.88%, 10%) 0%,
+  hsla(240, 5.88%, 10%, 0.738) 9.5%,
+  hsla(240, 5.88%, 10%, 0.541) 17%,
+  hsla(240, 5.88%, 10%, 0.382) 23.5%,
+  hsla(240, 5.88%, 10%, 0.278) 28.25%,
+  hsla(240, 5.88%, 10%, 0.194) 32.5%,
+  hsla(240, 5.88%, 10%, 0.126) 36.5%,
+  hsla(240, 5.88%, 10%, 0.075) 40.1%,
+  hsla(240, 5.88%, 10%, 0.042) 43.05%,
+  hsla(240, 5.88%, 10%, 0.021) 45.5%,
+  hsla(240, 5.88%, 10%, 0.008) 47.6%,
+  hsla(240, 5.88%, 10%, 0.002) 49.1%,
+  hsla(240, 5.88%, 10%, 0) 50%
+  );
+}
+
+#artist-gradient-left {
+  background: linear-gradient(-90deg,
+  hsl(240, 5.88%, 10%) 0%,
+  hsla(240, 5.88%, 10%, 0.738) 9.5%,
+  hsla(240, 5.88%, 10%, 0.541) 17%,
+  hsla(240, 5.88%, 10%, 0.382) 23.5%,
+  hsla(240, 5.88%, 10%, 0.278) 28.25%,
+  hsla(240, 5.88%, 10%, 0.194) 32.5%,
+  hsla(240, 5.88%, 10%, 0.126) 36.5%,
+  hsla(240, 5.88%, 10%, 0.075) 40.1%,
+  hsla(240, 5.88%, 10%, 0.042) 43.05%,
+  hsla(240, 5.88%, 10%, 0.021) 45.5%,
+  hsla(240, 5.88%, 10%, 0.008) 47.6%,
+  hsla(240, 5.88%, 10%, 0.002) 49.1%,
+  hsla(240, 5.88%, 10%, 0) 50%
+  );
+}
+
+</style>
--- backend/resources/js/pages/artist/Show.vue
+++ backend/resources/js/pages/artist/Show.vue
@@ -1,23 +1,13 @@
 <script setup lang="ts">
 import AlbumList from "@/components/albums/AlbumList.vue";
+import ArtistHero from "@/components/artists/ArtistHero.vue";
 
 defineProps<{ artist: { data: ExtendedArtist } }>();
 </script>
 
 <template>
-  <main class="max-w-7xl mx-auto">
-    <section class="w-full relative">
-      <div class="absolute inset-0 bg-gradient-to-t to-50% from-zinc-900 to-transparent"></div>
-      <div class="aspect-video w-full">
-        <img :src="artist.data.background_image" :alt="`${artist.data.name} background`" class="w-full h-full object-center object-cover">
-      </div>
-
-      <div class="absolute inset-x-0 bottom-10 mx-10">
-        <h1 class="text-6xl font-black text-white mb-4">
-          {{ artist.data.name }}
-        </h1>
-      </div>
-    </section>
+  <main class="container mx-auto">
+    <ArtistHero :artist="artist.data"/>
 
     <section class="pt-24 pb-12">
        <AlbumList :albums="artist.data.albums" :show-artist="false" :show-date="true"/>
