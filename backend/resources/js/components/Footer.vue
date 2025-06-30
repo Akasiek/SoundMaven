0cb2bf97a29ce9be3cfa04173d195b574f2b987b
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-06-30 21:47:49 +0200

    feat: Add Footer component and dynamic main height adjustment in AppLayout
    - Introduced a new Footer component with basic styling.
    - Updated AppLayout to calculate and apply dynamic height for the main section, accommodating footer and navigation heights.
    - Added `id` to Navigation for easier DOM queries.

---
+++ backend/resources/js/components/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <footer class="min-h-36 bg-zinc-850">
+    TEST
+  </footer>
+</template>
--- backend/resources/js/layouts/AppLayout.vue
+++ backend/resources/js/layouts/AppLayout.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
 import Navigation from "@/components/Navigation.vue";
 import PrimaryLayout from "@/layouts/PrimaryLayout.vue";
+import Footer from "@/components/Footer.vue";
+import { onMounted, ref } from "vue";
+
+const mainHeight = ref(0);
+
+const getHeight = (selector: string) => (document.querySelector(selector) as HTMLElement)?.offsetHeight || 0;
+const updateMainHeight = () => {
+  const vh = window.innerHeight;
+  mainHeight.value = vh - getHeight('#top-nav') - getHeight('footer');
+};
+
+onMounted(() => {
+  window.addEventListener('resize', updateMainHeight);
+  updateMainHeight();
+});
 </script>
 
 <template>
-    <PrimaryLayout>
-        <Navigation/>
-        <main class="mt-18 px-4 sm:px-6 lg:px-8">
-            <slot/>
-        </main>
-    </PrimaryLayout>
+  <PrimaryLayout>
+    <Navigation/>
+    <main class="mt-18 px-4 sm:px-6 lg:px-8" :style="{ minHeight: `${mainHeight ?? 0}px` }">
+      <slot/>
+    </main>
+    <Footer/>
+  </PrimaryLayout>
 </template>
-
-<style scoped>
-
-</style>
