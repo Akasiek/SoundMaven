4de78705cd0b3b3f9befc309359be285c0971eec
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-07-03 21:50:45 +0200

    feat: Improve SpecificAlbumReviewSeeder to associate reviews with users and prevent duplicate reviews
    - Added logic to associate each review with a user.
    - Ensured users cannot review the same album multiple times.
    - Dynamically created users when fewer than 20 exist in the database.

--- backend/database/seeders/SpecificAlbumReviewSeeder.php
+++ backend/database/seeders/SpecificAlbumReviewSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Album;
 use App\Models\AlbumReview;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class SpecificAlbumReviewSeeder extends Seeder
@@ -19,8 +20,29 @@ class SpecificAlbumReviewSeeder extends Seeder
 
         $count = (int)$this->command->ask('How many reviews would you like to create for this album?', 100);
 
-        AlbumReview::factory($count)->create([
-            'album_id' => $album->id,
-        ]);
+        foreach (range(1, $count) as $ignored) {
+            auth()->login($this->getUser($album));
+
+            AlbumReview::factory()->create([
+                'album_id' => $album->id,
+            ]);
+        }
+    }
+
+    private function getUser($album): User
+    {
+        $dbHasLessThenTwentyUsers = User::count() < 20;
+        if ($dbHasLessThenTwentyUsers) {
+            return User::factory()->create();
+        }
+
+        $user = User::inRandomOrder()->first();
+
+        // Check if the user already reviewed the album
+        if ($album->reviews()->where('created_by', $user->id)->exists()) {
+            return User::factory()->create();
+        }
+
+        return $user;
     }
 }
