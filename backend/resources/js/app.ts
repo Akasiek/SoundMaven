7cd08691a3477cd428ea5e188a6b01c80d683513
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-07-13 21:51:36 +0200

    feat: Integrate `vue-select` component and update dependencies
    - Added `vue-select` and its styles for improved dropdown functionality.
    - Registered `vue-select` as a global component in `app.ts`.
    - Updated dependencies: `@vueuse/core`, `reka-ui`, and package lock.

--- backend/resources/js/app.ts
+++ backend/resources/js/app.ts
@@ -4,39 +4,41 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../node_modules/ziggy-js';
+import VueSelect from "vue-select";
 import AppLayout from "@/layouts/AppLayout.vue";
 
 // Extend ImportMeta interface for Vite...
 declare module 'vite/client' {
-    interface ImportMetaEnv {
-        readonly VITE_APP_NAME: string;
+  interface ImportMetaEnv {
+    readonly VITE_APP_NAME: string;
 
-        [key: string]: string | boolean | undefined;
-    }
+    [key: string]: string | boolean | undefined;
+  }
 
-    interface ImportMeta {
-        readonly env: ImportMetaEnv;
-        readonly glob: <T>(pattern: string) => Record<string, () => Promise<T>>;
-    }
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+    readonly glob: <T>(pattern: string) => Record<string, () => Promise<T>>;
+  }
 }
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => title ? `${title} - ${appName}` : appName,
-    resolve: name => {
-        const pages = import.meta.glob<DefineComponent>('./pages/**/*.vue', {eager: true});
-        let page = pages[`./pages/${name}.vue`]
-        page.default.layout = page.default.layout === undefined ? AppLayout : page.default.layout;
-        return page
-    },
-    setup({el, App, props, plugin}) {
-        createApp({render: () => h(App, props)})
-            .use(plugin)
-            .use(ZiggyVue)
-            .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
-    },
+  title: (title) => title ? `${title} - ${appName}` : appName,
+  resolve: name => {
+    const pages = import.meta.glob<DefineComponent>('./pages/**/*.vue', { eager: true });
+    let page = pages[`./pages/${name}.vue`]
+    page.default.layout = page.default.layout === undefined ? AppLayout : page.default.layout;
+    return page
+  },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(ZiggyVue)
+      .component("v-select", VueSelect)
+      .mount(el);
+  },
+  progress: {
+    color: '#4B5563',
+  },
 });
