2219680bedff8b389ccce923bf03ba93ab4654b0
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-09-13 00:18:15 +0200

    refactor: Remove Inertia prefetch macro from AppServiceProvider
    - Deleted the `Inertia::prefetch` macro definition as it was replaced with a direct call to `Vite::prefetch`.

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
+use Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,18 +24,5 @@ class AppServiceProvider extends ServiceProvider
         JsonResource::withoutWrapping();
 
         Vite::prefetch(6);
-
-        Inertia::macro('prefetch', function (string|array $urls) {
-            if (!request()->inertia()) {
-                return $this;
-            }
-
-            $this->share('prefetch', [
-                ...$this->getShared('prefetch', []),
-                ...(array)$urls,
-            ]);
-
-            return $this;
-        });
     }
 }
