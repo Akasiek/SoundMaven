a1d59e23a63834c5cee29fa561ab956859e9bc17
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-09-29 23:26:35 +0200

    feat: Remove soft deletes from AlbumReview model and table
    - Dropped `SoftDeletes` trait from `Review` abstract model.
    - Removed soft delete columns from `album_reviews` table via migration.

--- backend/app/Models/Abstract/Review.php
+++ backend/app/Models/Abstract/Review.php
@@ -5,12 +5,11 @@ namespace App\Models\Abstract;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use RichanFongdasen\EloquentBlameable\BlameableTrait;
 
 abstract class Review extends Model
 {
-    use SoftDeletes, HasFactory, HasUuids, BlameableTrait;
+    use HasFactory, HasUuids, BlameableTrait;
 
     protected $perPage = 25;
 
