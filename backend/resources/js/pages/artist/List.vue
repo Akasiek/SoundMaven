193615faf571b03fa0a9c537fa25ba3e405e4b4d
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-12-15 20:34:17 +0100

    refactor: Adjust layout utility classes in Artist List component
    - Replaced `max-w-7xl` with `container` for consistent layout styling.

--- backend/resources/js/pages/artist/List.vue
+++ backend/resources/js/pages/artist/List.vue
@@ -6,17 +6,11 @@ defineProps<{ artists: { data: Artist[] } }>();
 </script>
 
 <template>
-  <main class="max-w-7xl mx-auto py-12">
+  <main class="container mx-auto py-12">
     <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-8 gap-y-8 md:gap-y-12">
       <template v-for="artist in artists.data" :key="artist.id">
         <ArtistCard :artist="artist"/>
       </template>
     </div>
-
   </main>
-
 </template>
-
-<style scoped>
-
-</style>
